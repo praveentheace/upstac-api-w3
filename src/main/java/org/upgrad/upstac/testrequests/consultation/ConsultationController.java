178db73b247d7c7cf8c9e7a2bc6da3820b1c76b3
Praveen <praveentheace@gmail.com>
2020-10-25 00:46:40 -0700

    complete ConsultationController

--- src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
+++ src/main/java/org/upgrad/upstac/testrequests/consultation/ConsultationController.java
@@ -8,10 +8,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.upgrad.upstac.config.security.UserLoggedInService;
 import org.upgrad.upstac.exception.AppException;
+import org.upgrad.upstac.testrequests.RequestStatus;
 import org.upgrad.upstac.testrequests.TestRequest;
 import org.upgrad.upstac.testrequests.TestRequestQueryService;
 import org.upgrad.upstac.testrequests.TestRequestUpdateService;
 import org.upgrad.upstac.testrequests.flow.TestRequestFlowService;
+import org.upgrad.upstac.users.User;
 
 import javax.validation.ConstraintViolationException;
 import java.util.List;
@@ -27,8 +29,6 @@ public class ConsultationController {
     Logger log = LoggerFactory.getLogger(ConsultationController.class);
 
 
-
-
     @Autowired
     private TestRequestUpdateService testRequestUpdateService;
 
@@ -37,39 +37,46 @@ public class ConsultationController {
 
 
     @Autowired
-    TestRequestFlowService  testRequestFlowService;
+    TestRequestFlowService testRequestFlowService;
 
     @Autowired
     private UserLoggedInService userLoggedInService;
 
 
-
     @GetMapping("/in-queue")
     @PreAuthorize("hasAnyRole('DOCTOR')")
-    public List<TestRequest> getForConsultations()  {
+    public List<TestRequest> getForConsultations() {
         //Implement this method to get the list of test requests having status as 'LAB_TEST_COMPLETED'
         // make use of the findBy() method from testRequestQueryService class
         //return the result
         // For reference check the method requestHistory() method from TestRequestController class
-        return null; // replace this line with your code
+        try {
+            List<TestRequest> testRequests = testRequestQueryService.findBy(RequestStatus.LAB_TEST_COMPLETED);
+            return testRequests;
+        } catch (AppException e) {
+            throw asBadRequest(e.getMessage());
+        }
 
     }
 
     @GetMapping
     @PreAuthorize("hasAnyRole('DOCTOR')")
-    public List<TestRequest> getForDoctor()  {
+    public List<TestRequest> getForDoctor() {
 
         // Create an object of User class and store the current logged in user first
         //Implement this method to return the list of test requests assigned to current doctor(make use of the above created User object)
         //Make use of the findByDoctor() method from testRequestQueryService class to get the list
         // For reference check the method getPendingTests() method from TestRequestController class
-
-        return null; // replace this line with your code
-
+        try {
+            User user = userLoggedInService.getLoggedInUser();
+            List<TestRequest> testRequests = testRequestQueryService.findByDoctor(user);
+            return testRequests;
+        } catch (AppException e) {
+            throw asBadRequest(e.getMessage());
+        }
 
 
     }
-
 
 
     @PreAuthorize("hasAnyRole('DOCTOR')")
@@ -81,32 +88,34 @@ public class ConsultationController {
         // return the above created object
         // Refer to the method createRequest() from the TestRequestController class
         try {
-            return null; // replace this line of code with your implementation
-        }catch (AppException e) {
+            User user = userLoggedInService.getLoggedInUser();
+            TestRequest testRequest = testRequestUpdateService.assignForConsultation(id, user);
+            return testRequest;
+        } catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
 
 
-
     @PreAuthorize("hasAnyRole('DOCTOR')")
     @PutMapping("/update/{id}")
-    public TestRequest updateConsultation(@PathVariable Long id,@RequestBody CreateConsultationRequest testResult) {
+    public TestRequest updateConsultation(@PathVariable Long id, @RequestBody CreateConsultationRequest testResult) {
         // Implement this method to update the result of the current test request id with test doctor comments
         // Create an object of the User class to get the logged in user
         // Create an object of TestResult class and make use of updateConsultation() method from testRequestUpdateService class
         //to update the current test request id with the testResult details by the current user(object created)
 
         try {
-            return null; // replace this line of code with your implementation
+            User doctor = userLoggedInService.getLoggedInUser();
+            TestRequest testRequest = testRequestUpdateService.updateConsultation(id, testResult, doctor);
+            return testRequest;
 
         } catch (ConstraintViolationException e) {
             throw asConstraintViolation(e);
-        }catch (AppException e) {
+        } catch (AppException e) {
             throw asBadRequest(e.getMessage());
         }
     }
-
 
 
 }
